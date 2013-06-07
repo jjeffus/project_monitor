3521a40ae490d7d705169fc3c06375e24f74c242
David Collazo <davidcollazo@gmail.com>
2013-06-07 14:13:18 -0700

    Devise methods issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  field :name, type: String, default: ""
+  field :email, type: String, default: ""
+  field :password, type: String, default: ""
+  field :password_confirmation, type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+  field :remember_me, type: Boolean, default: false
 end
